d996286a797a32a8fc3e6d76510ed4e47a5a0b99
marco89 <63103599+marco89@users.noreply.github.com>
2021-04-08 13:35:58 +0100

    Add files via upload
    First real iteration, ability to convert from GBP to USD only.

--- Currency Converter.py
+++ Currency Converter.py
@@ -1,10 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Requests HTML from source website
 page = requests.get('https://www.x-rates.com/table/?from=GBP&amount=1')
 
+# Converts and parses HTML to usable data
 soup = BeautifulSoup(page.content, 'html.parser')
 
-tds = soup.find_all('td', attrs={'class': 'rtRates'})
+# Finds relevant data types needed which is all currency to currency figures (those that begin with rtRates)
+soup_figure = soup.find_all('td', attrs={'class': 'rtRates'})
 
-print(tds[0])
+# Selects the required currency from list, in this case GBP to USD
+initial_rate = str(soup_figure[0])
+
+# Cuts away all unneeded text from previous selection, also converts to float
+rate = float(str(initial_rate[81:88]))
+
+# Asks user input in float form
+gbp_input = float(input('Please input amount of GBP you would like to convert to USD:'))
+
+# Multiplies user input figure by current exchange rate
+conv = round(gbp_input * rate, 2)
+
+print('£', gbp_input, 'is', '$', conv)
+
